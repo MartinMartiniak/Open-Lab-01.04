f90daf924eaec13bca21d13d35f9a8767a917a72
Martin Martiniak <martiniak.martin15@gmail.com>
2020-10-05 07:30:31 +0200

    Inserted formula for Area of triangle and returned it.

--- Open-Lab-01.04/Triangle.cs
+++ Open-Lab-01.04/Triangle.cs
@@ -6,7 +6,8 @@ namespace Open_Lab_01._04
     {
         public double Area(double foundation, double height)
         {
-            throw new NotImplementedException();
+            double area = foundation * height / 2;
+            return area;
         }
     }
 }
